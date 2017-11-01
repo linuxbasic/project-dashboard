f03dbc75c99a7dbe7c678716983cbb2a75048c9b
Linus Basig <linus@basig.me>
2017-11-01 20:04:30 +0100

    Reduce raster for Milestone Trend Anaysis

--- dashboard/templatetags/mta.py
+++ dashboard/templatetags/mta.py
@@ -9,7 +9,7 @@ date_template = 'new Date("{}")'
 def get_days(start_date, end_date):
     delta = end_date - start_date
 
-    return [start_date + timedelta(days=i) for i in range(delta.days + 1)]
+    return [start_date + timedelta(days=i) for i in range(0, delta.days + 1, 7)]
 
 
 def to_json_array(array):
--- dashboard/tests/templatetags/test_mta.py
+++ dashboard/tests/templatetags/test_mta.py
@@ -9,8 +9,7 @@ from core.shared.test_utils import create_project, create_phase, create_task
 class MTATemplatetagsTests(TestCase):
     def test_should_return_list_of_dates(self):
         START_DATE = date(2017, 10, 1)
-        END_DATE = date(2017, 10, 6)
-        EXPECED_DATES = [date(2017, 10, 1), date(2017, 10, 2), date(2017, 10, 3), date(2017, 10, 4), date(2017, 10, 5),
-                         date(2017, 10, 6), ]
+        END_DATE = date(2017, 10, 16)
+        EXPECED_DATES = [date(2017, 10, 1), date(2017, 10, 8), date(2017, 10, 15), ]
 
         self.assertListEqual(get_days(START_DATE, END_DATE), EXPECED_DATES)
