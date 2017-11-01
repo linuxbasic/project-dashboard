2823c300c6bcafe7b8c347efde9a9a693cae0d3b
Linus Basig <linus@basig.me>
2017-11-01 12:45:23 +0100

    Split TaskTests into multiple files

--- project/tests/task/test_cost.py
+++ project/tests/task/test_cost.py
@@ -5,38 +5,7 @@ from django.test import TestCase
 from project.tests.utils import create_task
 
 
-class TaskTests(TestCase):
-    def test_should_return_planned_duration(self):
-        PLANNED_DURATION = 5
-        task = create_task(planned_duration=PLANNED_DURATION)
-        self.assertEqual(task.get_planned_duration(), PLANNED_DURATION)
-
-    def test_should_return_planned_duration_if_real_not_available(self):
-        PLANNED_DURATION = 5
-        task = create_task(planned_duration=PLANNED_DURATION)
-        self.assertEqual(task.get_duration(), PLANNED_DURATION)
-
-    def test_should_return_predicted_duration_if_available(self):
-        PLANNED_DURATION = 1
-        PREDICTED_DURATION = 5
-        task = create_task(planned_duration=PLANNED_DURATION)
-        task.duration_predictions.create(date=date.today() + timedelta(days=2), duration=PREDICTED_DURATION)
-        self.assertEqual(task.get_duration(), PREDICTED_DURATION)
-
-    def test_should_return_predicted_duration_on_specific_date(self):
-        PLANNED_DURATION = 5
-        PREDICTED_DURATION_1 = 3
-        PREDICTED_DURATION_2 = 2
-
-        task = create_task(planned_duration=PLANNED_DURATION)
-        task.duration_predictions.create(date=date.today() + timedelta(days=1), duration=PREDICTED_DURATION_1)
-        task.duration_predictions.create(date=date.today() + timedelta(days=3), duration=PREDICTED_DURATION_2)
-
-        self.assertEqual(task.get_duration(date.today() + timedelta(days=1)), PREDICTED_DURATION_1)
-        self.assertEqual(task.get_duration(date.today() + timedelta(days=2)), PREDICTED_DURATION_1)
-        self.assertEqual(task.get_duration(date.today() + timedelta(days=3)), PREDICTED_DURATION_2)
-        self.assertEqual(task.get_duration(date.today() + timedelta(days=4)), PREDICTED_DURATION_2)
-
+class TaskCostTests(TestCase):
     def test_should_return_planned_cost_of_single_resource(self):
         PLANNED_DURATION = 5
         RESOURCE_COST = 50
