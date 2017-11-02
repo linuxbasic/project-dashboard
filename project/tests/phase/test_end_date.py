d2c74edde242a5cd8cb75d73aaf52ddacb70d9b1
Linus Basig <linus@basig.me>
2017-11-02 15:48:30 +0100

    Refactor end_date tests for phase

--- project/tests/phase/test_end_date.py
+++ project/tests/phase/test_end_date.py
@@ -11,14 +11,7 @@ class PhaseEndDateTests(TestCase):
         phase1 = create_phase(project=project)
         create_task(phase=phase1, planned_duration=TASK_DURATION)
         self.assertEqual(phase1.get_planned_end_date(), START_DATE + timedelta(days=TASK_DURATION))
-
-    def test_should_return_planned_start_date_if_no_duration_predictions_available(self):
-        START_DATE = date(2017, 10, 25)
-        PLANNED_DURATION = 2
-        project = create_project(start_date=START_DATE)
-        phase1 = create_phase(project=project)
-        create_task(phase=phase1, planned_duration=PLANNED_DURATION)
-        self.assertEqual(phase1.get_end_date(), START_DATE + timedelta(days=PLANNED_DURATION))
+        self.assertEqual(phase1.get_end_date(), START_DATE + timedelta(days=TASK_DURATION))
 
     def test_should_return_predicted_end_date(self):
         START_DATE = date(2017, 10, 25)
@@ -39,14 +32,14 @@ class PhaseEndDateTests(TestCase):
         phase1 = create_phase(project=project)
         task = create_task(phase=phase1, planned_duration=PLANNED_DURATION)
 
-        task.duration_predictions.create(date=date.today() + timedelta(days=1), duration=PREDICTED_DURATION_1)
-        task.duration_predictions.create(date=date.today() + timedelta(days=3), duration=PREDICTED_DURATION_2)
+        task.duration_predictions.create(date=START_DATE + timedelta(days=1), duration=PREDICTED_DURATION_1)
+        task.duration_predictions.create(date=START_DATE + timedelta(days=3), duration=PREDICTED_DURATION_2)
 
-        self.assertEqual(phase1.get_end_date(date.today() + timedelta(days=1)),
+        self.assertEqual(phase1.get_end_date(START_DATE + timedelta(days=1)),
                          START_DATE + timedelta(days=PREDICTED_DURATION_1))
-        self.assertEqual(phase1.get_end_date(date.today() + timedelta(days=2)),
+        self.assertEqual(phase1.get_end_date(START_DATE + timedelta(days=2)),
                          START_DATE + timedelta(days=PREDICTED_DURATION_1))
-        self.assertEqual(phase1.get_end_date(date.today() + timedelta(days=3)),
+        self.assertEqual(phase1.get_end_date(START_DATE + timedelta(days=3)),
                          START_DATE + timedelta(days=PREDICTED_DURATION_2))
-        self.assertEqual(phase1.get_end_date(date.today() + timedelta(days=4)),
+        self.assertEqual(phase1.get_end_date(START_DATE + timedelta(days=4)),
                          START_DATE + timedelta(days=PREDICTED_DURATION_2))
