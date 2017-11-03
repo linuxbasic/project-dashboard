fee8912e7f622fde5a2686e477f4d58cbe68b92d
Linus Basig <linus@basig.me>
2017-11-03 20:57:12 +0100

    Move status calculation to dashboard app

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,12 +3,28 @@ from project.models import Project
 from datetime import date
 
 
-# Create your views here.
+def calculate_time_status(project):
+    planned_duration = project.get_planned_duration()
+    duration = project.get_duration()
+    delta = planned_duration - duration
+    if delta >= 0:
+        return 1
+    missmatch = abs(planned_duration / duration - 1)
+    if missmatch <= 0.05:
+        return 2
+    return 3
 
 
 def index(request):
     project = Project.objects.all().prefetch_related('phases__tasks__duration_predictions').prefetch_related(
         'phases__tasks__resources').last()
-    context = {'project': project, 'today': date.today()}
+    today = date.today()
+    context = {
+        'project': project,
+        'today': today,
+        'status': {
+            'time': calculate_time_status(project)
+        }
+    }
 
     return render(request=request, template_name='dashboard/dashboard.html', context=context)
--- project/models/project.py
+++ project/models/project.py
@@ -1,18 +1,7 @@
-from django.db import models
 from datetime import timedelta
+from django.db import models
 
 
-def calculate_time_status(planned_duration, duration):
-    delta = planned_duration - duration
-    if delta >= 0:
-        return 1
-    missmatch = abs(planned_duration / duration - 1)
-    if missmatch <= 0.05:
-        return 2
-    return 3
-
-
-# Create your models here.
 class Project(models.Model):
     name = models.CharField(verbose_name='Project Name', max_length=50, )
     start_date = models.DateField(verbose_name='Start Date', )
@@ -28,11 +17,6 @@ class Project(models.Model):
 
     def get_end_date(self):
         return self.start_date + timedelta(days=self.get_duration())
-
-    def get_time_status(self):
-        planned_duration = self.get_planned_duration()
-        duration = self.get_duration()
-        return calculate_time_status(planned_duration, duration)
 
     def get_planned_cost(self, on_date=None):
         return sum([phase.get_planned_cost(on_date) for phase in self.phases.all()])
