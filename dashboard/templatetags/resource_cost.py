59ca6ce4d3119b41ea33659150137b4cddcbf5ef
Linus Basig <linus@basig.me>
2017-11-06 10:37:43 +0100

    Fix resource cost in dashboard

--- dashboard/templatetags/resource_cost.py
+++ dashboard/templatetags/resource_cost.py
@@ -17,18 +17,18 @@ def resource_duration(resource, project):
 
 @register.filter
 def planned_resource_cost(resource, project):
-    return sum([phase.get_planned_cost() for phase in resource.tasks.filter(phase__project=project) if
+    return sum([phase.get_planned_duration() * resource.cost for phase in resource.tasks.filter(phase__project=project) if
                 phase.get_end_date() <= project.today()])
 
 
 @register.filter
 def resource_cost(resource, project):
-    return sum([phase.get_cost() for phase in resource.tasks.filter(phase__project=project) if
+    return sum([phase.get_duration() * resource.cost for phase in resource.tasks.filter(phase__project=project) if
                 phase.get_end_date() <= project.today()])
 
 
 @register.filter
-def cost_delta(resource, project):
+def resource_cost_delta(resource, project):
     planned_cost = planned_resource_cost(resource, project)
     actual_cost = resource_cost(resource, project)
     return actual_cost / planned_cost
